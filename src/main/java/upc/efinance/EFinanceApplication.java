26590126d2f3ffbf62d2c1b5fa0e760bbbf6d196
Gustavo <u20221c138@upc.edu.pe>
2024-11-19 00:52:26 -0500

    Update EFinanceApplication.java

--- src/main/java/upc/efinance/EFinanceApplication.java
+++ src/main/java/upc/efinance/EFinanceApplication.java
@@ -2,15 +2,31 @@ package upc.efinance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class EFinanceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EFinanceApplication.class, args);
     }
 
-}
+    @Configuration
+    public static class MyConfiguration {
 
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+                }
+            };
+        }
+    }
+}
